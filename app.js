32b4171b1b6d463b1d3cb74cfe3f17ee58ed45b3
Johannes Dillmann <j.dillmann@sap.com>
2021-03-05 10:06:21 +0100

    Add log

--- app.js
+++ app.js
@@ -5,36 +5,38 @@ var express = require('express'),
 var port = process.env.PORT || 8089;
 
 
-setTimeout(() => {
-    var io = require('socket.io').listen(app.listen(port));
+var io = require('socket.io').listen(app.listen(port));
 
-    app.set('view engine', 'html');
-    app.engine('html', require('ejs').renderFile);
-    app.set('views', __dirname + '/views');
-    app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
-    require('./routes')(app, io);
+require('./routes')(app, io);
 
-    console.log('Your application is running on http://localhost:' + port);
+console.log('Your application is running on http://localhost:' + port);
 
-    // The signals we want to handle
-    // NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
-    var signals = {
-        'SIGHUP': 1,
-        'SIGINT': 2,
-        'SIGTERM': 15
-    };// Do any necessary shutdown logic for our application here
-    const shutdown = (signal, value) => {
-        console.log("shutdown!");
-        io.close(() => {
-            console.log(`server stopped by ${signal} with value ${value}`);
-            process.exit(128 + value);
-        });
-    };// Create a listener for each of the signals that we want to handle
-    Object.keys(signals).forEach((signal) => {
-        process.on(signal, () => {
-            console.log(`process received a ${signal} signal`);
-            shutdown(signal, signals[signal]);
-        });
+setInterval(() => {
+    console.log('Ping http://localhost:' + port);
+}, 1000)
+
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+var signals = {
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
+};// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+    console.log("shutdown!");
+    io.close(() => {
+        console.log(`server stopped by ${signal} with value ${value}`);
+        process.exit(128 + value);
     });
-}, 40000)
+};// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+        console.log(`process received a ${signal} signal`);
+        shutdown(signal, signals[signal]);
+    });
+});
